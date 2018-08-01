9b66154afb67c325aaed6aa25ebace8bd1442095
Shane Procter <farmy4700@gmail.com>
2018-07-31 19:47:17 -0500

    lesson updates

--- complete-javascript-course-master/2-JS-basics/starter/script.js
+++ complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -1,3 +1,8 @@
+/***********************
+This lecture is about variables and datatypes
+************************/
+
+/*
 var firstName = 'John';
 console.log(firstName);
 
@@ -12,3 +17,93 @@ console.log(job);
 
 job = 'Teacher';
 console.log(job);
+
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'John and Mark';
+var if 23;
+
+
+/**************************
+ * Variable Mutation and type coercion
+ **************************/
+/*
+var firstName = 'John';
+age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//VARIABLE MUTATION
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+/**************************************
+ * Basic Operators
+ **************************************/
+
+var now, yearJohn, yearJohn, ageJohn, ageMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+yearJohn = now - ageJohn; 
+yearMark = now - ageMark;
+console.log(yearJohn);
+//Math Operators
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+
+//Logical Operators
+if (ageMark > ageJohn) {
+    console.log("John is the oldest");
+}
+else {
+    console.log("Mark is the oldest");
+}
+
+var johnOlder = ageMark < ageJohn;
+console.log(johnOlder);
+
+//typeof operator
+console.log(typeof johnOlder);
+console.log(typeof now);
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
