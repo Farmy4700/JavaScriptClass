10dc159118a7ef10408b9bad658bfa777b4f5f5f
Shane Procter <shane.procter@anpac.com>
2018-08-07 13:43:14 -0500

    more lessons

--- complete-javascript-course-master/3-how-JS-works/starter/script.js
+++ complete-javascript-course-master/3-how-JS-works/starter/script.js
@@ -1,6 +1,6 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
-
+/*
 //funtions
 function calculateAge(year) { //function declaration
     console.log(2018 - year);
@@ -25,13 +25,7 @@ function foo() {
 }
 foo();
 console.log(age);
-
-
-
-
-
-
-
+*/
 
 ///////////////////////////////////////
 // Lecture: Scoping
@@ -40,16 +34,16 @@ console.log(age);
 // First scoping example
 
 /*
-var a = 'Hello!';
-first();
+var a = 'Hello!';                //Global scope 
+first();                         //global scope 
 
-function first() {
-    var b = 'Hi!';
-    second();
+function first() {               
+    var b = 'Hi!';               //function scope
+    second();                    //function scope 
 
     function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
+        var c = 'Hey!';             //scope of second function
+        console.log(a + b + c);     //scope of second function 
     }
 }
 */
@@ -74,14 +68,51 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log(a + b + c + d);  //can only access a and d
 }
 */
 
 
-
 ///////////////////////////////////////
 // Lecture: The this keyword
+
+//Rugular funciton call:  the this keyworkd points at the global object, the windows object in the browser
+//Method Call: the this variable points to the object that is calling the method
+
+//The this keyword is not assigned a value until a function where it is definied is actually called
+
+//console.log(this);
+/*
+calculateAge(1985);
+
+function calculateAge(year) {
+    console.log(2018 - year);
+    console.log(this);  //still pointing to global object(regualr function call)
+}
+*/
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2018 - this.yearOfBirth);
+/*
+        function innerFunction() {
+            console.log(this);  //now it is back to window - this is a regular function
+        }
+        innerFunction(); */
+    }
+}
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge;  //now we are borrow johns calculateAge function to be used for Mike
+
+mike.calculateAge();
 
 
 
