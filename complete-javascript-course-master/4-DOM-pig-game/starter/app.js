c9fcc3224234ea516489e6d6fa9cfcd685104d59
Shane Procter <shane.procter@anpac.com>
2018-08-15 14:35:05 -0500

    new game button

--- complete-javascript-course-master/4-DOM-pig-game/starter/app.js
+++ complete-javascript-course-master/4-DOM-pig-game/starter/app.js
@@ -10,16 +10,9 @@ GAME RULES:
 */
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -59,7 +52,6 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 });
 
 
-
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;  //same as if active player 0, then 1, else 0
     roundScore = 0;
@@ -77,6 +69,29 @@ function nextPlayer() {
 
 }
 
+document.querySelector('.btn-new').addEventListener('click', init();
+
+function init() {
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector('.dice').style.display = 'noe';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+}
 
 
 
